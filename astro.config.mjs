3433e755e5b8dcaf2466d05753e88124ae058134
Nicholas Young <hi@secretfader.com>
2025-05-08 12:00:45 -0600

    feat: update to the latest astro options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+const site = import.meta.env.CF_PAGES_URL || "https://fictiveflame.co";
 
 // https://astro.build/config
 export default defineConfig({
+  site,
   vite: { plugins: [tailwind()] },
+  integrations: [sitemap()],
+  adapter: cloudflare(),
 });
