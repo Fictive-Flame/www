889321fd05bebbd2c3d22fc04ac8ac3938c66908
Nicholas Young <hi@secretfader.com>
2025-05-10 17:24:10 -0600

    feat: add work videos and categories

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,3 +3,14 @@ import { getCollection } from "astro:content";
 export async function getWork() {
   return await getCollection("work");
 }
+
+export function slugify(input: string) {
+  input = input.replace(/^\s+|\s+$/g, "");
+  input = input.toLowerCase();
+  input = input
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+
+  return input;
+}
