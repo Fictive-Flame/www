976c2cecdee4aaa2b61cbb5e5793ab104905c556
Nicholas Young <hi@secretfader.com>
2025-05-19 13:07:39 -0600

    feat: add rss feed

---
+++ src/pages/blog/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import { getPosts } from "../../utils/data";
+
+export async function GET(ctx) {
+  const posts = await getPosts();
+
+  return rss({
+    title: `Fictive Flame Blog`,
+    description: `Education, insight, and behind-the-scenes content from a video production company.`,
+    site: new URL(`/blog`, ctx.site),
+    xmlns: {
+      atom: "https://www.w3.org/2005/Atom",
+    },
+    items: postsToRSSItems(posts),
+  });
+}
+
+const postsToRSSItems = (posts) =>
+  posts.map(({ id, data, rendered }) => ({
+    title: data.title,
+    description: data.description,
+    link: `/blog/${id}`,
+    content: rendered.html,
+  }));
